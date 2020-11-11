32e8ad64090c0167260a76681657cd212c4c0624
piotr-ost <piotr.ostrowski@aol.com>
2020-11-11 20:54:43 +0100

    meal plan styling - wip

--- screens/MealPlanScreen.js
+++ screens/MealPlanScreen.js
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Image, Button} from 'react-native';
+import {View, Text, Image, Button, StyleSheet} from 'react-native';
+import {Divider, Icon} from 'react-native-elements';
 import axios from 'axios';
+
+const ReturnButton = ({onPress}) => {
+  return (
+    <Icon onPress={onPress} type="font-awesome"
+          name="arrow-left" color="green" />
+  )
+}
 
 const ShoppingListButton = ({recipes}) => {
   // todo get the products using an api call
@@ -9,7 +17,8 @@ const ShoppingListButton = ({recipes}) => {
   const shoppingAction = () => {
     recipes.forEach((id) => {
       try {
-        const apiKey = '?apiKey=556d5c003785468ab5aa696a128a3d3a';
+        // const apiKey = '?apiKey=556d5c003785468ab5aa696a128a3d3a';
+        const apiKey = '?apiKey=5bb1646af40448c4bd763b79205bc198'
         const url = `https://api.spoonacular.com/recipes/${id}/information`;
         console.log(url);
         const res = axios.get(url + apiKey);
@@ -23,8 +32,10 @@ const ShoppingListButton = ({recipes}) => {
     });
   }
   return (recipes.length === 7*3 ?
-      <Button onPress={shoppingAction} color="green" title="Shopping list for the week" />
-      : <Button onPress={() => {}} disabled title="Shopping list for the week" />
+      <Icon type="font-awesome" name="shopping-cart"
+            onPress={shoppingAction} color="green" />
+      : <Icon type="font-awesome" name="shopping-cart"
+              onPress={shoppingAction} color="green" />
   )
 }
 const Meal = ({id, imageType, title, readyInMinutes, servings}) => {
@@ -33,6 +44,7 @@ const Meal = ({id, imageType, title, readyInMinutes, servings}) => {
   return ( id && title ?
     <View>
       <Text>{title}</Text>
+      <Divider style={styles.grayDivider}/>
       <View>
         <Image style={{width: 100, height: 100}} source={{uri: uri}} />
       </View>
@@ -63,13 +75,13 @@ const MealsDay = ({dayName, meals, nutrients}) => {
 
 const MealPlanScreen = () => {
   const base = 'https://api.spoonacular.com/mealplanner/generate';
-  const apiKey = '?apiKey=556d5c003785468ab5aa696a128a3d3a';
+  // const apiKey = '?apiKey=556d5c003785468ab5aa696a128a3d3a';
+  const apiKey = '?apiKey=5bb1646af40448c4bd763b79205bc198'
   const [data, setData] = useState([]);
   const recipes = [];
 
   useEffect(() => {
     const fetchData = async () => {
-      console.log('firing a request');
       try {
         const res = await axios.get(base + apiKey);
         console.log('result:', res);
@@ -78,12 +90,17 @@ const MealPlanScreen = () => {
         console.log(err);
       }
     }
-    fetchData();
+    //fetchData();
   }, []);
 
   return (
-    <View>
-      <ShoppingListButton recipes={recipes}/>
+    <View style={styles.screen}>
+      <View style={styles.header}>
+        <ReturnButton onPress={() => {}} />
+        <Text style={styles.heading}>Meal Plan</Text>
+        <ShoppingListButton recipes={recipes}/>
+      </View>
+      <Divider style={{width:330, height: 3, backgroundColor: '#848484'}}/>
       <View>
         {data.week ?
           Object.values(data.week).map(({meals, nutrients}, index) => {
@@ -98,5 +115,20 @@ const MealPlanScreen = () => {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {display: 'flex', flexDirection: 'column'},
+  mealImg: {},
+  header: {
+    width: 300, height: 60, display: 'flex',
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: 'center', margin: 10
+  },
+  heading: {fontSize: 30},
+  mealLabel: {},
+  greenDivider: {width: 330, height: 2, backgroundColor: 'green'},
+  grayDivider: {width: 330, height: 2, backgroundColor: 'gray'}
+});
 
 export default MealPlanScreen;
