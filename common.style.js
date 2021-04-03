9ed5447bc07770e0e4e4f91d72f7ffa73f495153
Piotr Ostrowski <piotr.ostrowski@aol.com>
2021-04-03 22:53:32 +0200

    add common styles to use globally

---
+++ common.style.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+
+export default StyleSheet.create({
+    screen: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        paddingVertical: 10,
+        paddingHorizontal: 27,
+        justifyContent: 'space-between'
+    },
+    header: {
+        height: 45,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: 'center',
+        margin: 10,
+        marginTop: 30
+    },
+    flexRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    column: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+})
+
+export const GREEN = '#6FBF44'
