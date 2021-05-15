6e136d3eaf1634ad1075a39ef9e99cc4066c6e79
Piotr Ostrowski <piotr.ostrowski@aol.com>
2021-05-15 18:53:03 +0200

    fix the disappearing bug

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -7,7 +7,6 @@ import CardStack, { Card } from 'react-native-card-stack-swiper'
 
 const SwipeScreen = ({ route, navigation }) => {
   const [mealPlans, setMealPlans] = useState([])
-  const [swiper, setSwiper] = useState(null)
   useEffect(() => {
     fetch('https://handler.health/meal-plans')
       .then(r => r.json())
@@ -15,29 +14,36 @@ const SwipeScreen = ({ route, navigation }) => {
       .then(console.log(mealPlans))
   }, [])
   return (
-    <View > 
-      <CardStack
-        loop={true}
-        verticalSwipe={false}
-        renderNoMoreCards={() => null}
-        ref={swiper => setSwiper(swiper)}
-        cardContainerStyle={{width: '100%', height: '100%'}}
-      >
-        {
-          mealPlans.map((mealPlan, index) => (
-            <Card key={index}> 
-              <MealPlanCard 
-                mealPlan={mealPlan} 
-                onPressLeft={() => swiper.swipeLeft()}
-                onPressRight={() => swiper.swipeRight()}
-              />
-            </Card>
-          ))
-        }
-      </CardStack>
+    <View> 
+      { mealPlans.length
+          ? <SwipeBit mealPlans={mealPlans} />
+          : <Text>Chuj</Text>
+      }
     </View>
   )
 }
 
+const SwipeBit = ({ mealPlans }) => {
+  return (
+    <View >
+      <CardStack
+        loop={true}
+        verticalSwipe={false}
+        renderNoMoreCards={() => null}
+        cardContainerStyle={{width: '100%', height: '100%'}}
+      >
+      {
+        mealPlans.map((mealPlan, index) => (
+          <Card key={index}> 
+            <MealPlanCard 
+              mealPlan={mealPlan} 
+            />
+          </Card>
+        ))
+      }
+      </CardStack>
+    </View>
+  )
+}
 export default SwipeScreen
 
